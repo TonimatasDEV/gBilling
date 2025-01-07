f8c207340832ac3814adaba3c87d0e3f3ab03f18
TonimatasDEV <tonimatasdev@proton.me>
2025-01-07 21:04:35 +0100

    chore(templates): Extract page builder

--- main.go
+++ main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/TonimatasDEV/EtheneBillingPanel/src"
+	"github.com/TonimatasDEV/EtheneBillingPanel/src/pages"
 	"net/http"
 )
 
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-	http.HandleFunc("/", src.Handler)
+	http.HandleFunc("/", src.IndexHandler)
 
 	fmt.Println("Servidor iniciado en http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
