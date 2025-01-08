3223828aea297d2b7e1dee99a5ecc4c1d4821bc8
TonimatasDEV <tonimatasdev@proton.me>
2025-01-08 20:41:42 +0100

    feat(auth): Add login page

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"github.com/TonimatasDEV/EtheneBillingPanel/src/pages"
+	"log"
 	"net/http"
 )
 
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
-	http.HandleFunc("/", src.IndexHandler)
+	http.HandleFunc("/", auth(src.IndexHandler))
+	http.HandleFunc("/login", src.LoginHandler)
 
-	fmt.Println("Servidor iniciado en http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	log.Println("Starting the server with the port 8080.")
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		log.Fatal("Server crashed: ", err)
+		return
+	}
+}
+
+func auth(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("session_token")
+		if err != nil || cookie.Value != "some_secure_session_token" {
+			http.Redirect(w, r, "/login", http.StatusSeeOther)
+			return
+		}
+		next(w, r)
+	}
 }
