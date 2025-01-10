13534f919f6b6755382714787bdef95e3ceadcba
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2025-01-10 19:01:40 +0100

    feat(tables): Add more user information.

--- src/database/tables.go
+++ src/database/tables.go
@@ -20,7 +20,23 @@ func users() {
 	CREATE TABLE IF NOT EXISTS users (
 		id INT AUTO_INCREMENT PRIMARY KEY,
 		email VARCHAR(255) NOT NULL UNIQUE,
-		password_hash VARCHAR(255) NOT NULL
+		password VARCHAR(255) NOT NULL
+	    
+	    first_name VARCHAR(100) NOT NULL,
+		last_name VARCHAR(100) NOT NULL,
+		phone_number VARCHAR(20) NOT NULL,
+		verified BOOLEAN NOT NULL DEFAULT FALSE,
+		country VARCHAR(100) NOT NULL,
+		country_state VARCHAR(100) NOT NULL,
+		city VARCHAR(100) NOT NULL,
+		postal_code VARCHAR(20) NOT NULL,
+		address VARCHAR(255) NOT NULL,
+
+		lang VARCHAR(20) NOT NULL,
+		announcements BOOLEAN NOT NULL DEFAULT FALSE,
+
+		organization VARCHAR(255),
+		two_factor_auth BOOLEAN NOT NULL DEFAULT FALSE
 	);`
 
 	createTable(query)
