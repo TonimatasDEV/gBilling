b84eb48e5ea6a3333775c0d407a1a1983a26670a
TonimatasDEV <79017964+TonimatasDEV@users.noreply.github.com>
2025-04-05 17:13:01 +0200

    feat: Improve error api

---
+++ internal/domain/message.go
@@ -0,0 +1,26 @@
+package domain
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Msg struct {
+	Msg string `json:"msg"`
+}
+
+func SendError(w http.ResponseWriter, err error) {
+	create(err.Error()).send(w)
+}
+
+func SendString(w http.ResponseWriter, str string) {
+	create(str).send(w)
+}
+
+func create(str string) *Msg {
+	return &Msg{Msg: str}
+}
+
+func (msg *Msg) send(w http.ResponseWriter) {
+	_ = json.NewEncoder(w).Encode(msg)
+}
